c4cc4ef625d4ad1649fb8c532c780590b695ac55
MeisterBier <fluffyboy69@protonmail.com>
2024-01-09 21:57:26 +0100

    Projekt und Karte.java erstellt

---
+++ Karte.java
@@ -0,0 +1,11 @@
+public class Karte {
+    public final int wert, farbe;
+
+    public Karte(final int pWert, final int pFarbe) {
+            wert = pWert;
+            farbe = pFarbe;
+            //0 Ist ein Narr, 1-13 sind die Zahlenkarten, 14 ist der Zauberer
+            //0 - rot, 1 - gelb, 2- gruen, 3 - blau
+    }
+
+}
